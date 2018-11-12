3177b14888ca3b4d2d4af98f687339731affc6d9
Greg <sephiroth198@googlemail.com>
2018-11-12 01:35:45 +0000

    Basic Demonstration
    Added:
    Terrain generation
    Controllable agent
    Navmesh
    Camera follow

---
+++ Pathfinding-Honours/Assets/FollowAgent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FollowAgent : MonoBehaviour {
+
+    public Transform agent;
+    public Vector3 offset;
+
+	// Update is called once per frame
+	private void Update () {
+        transform.position = agent.position + offset;
+
+	}
+}
---
+++ Pathfinding-Honours/Assets/PerlinNoise.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEngine;
+
+public class PerlinNoise : MonoBehaviour {
+
+    public int width = 256, height = 256;
+    public float scale = 20, offsetX =100.0f, offsetY=100.0f;
+
+    private void Start()
+    {
+        offsetX = Random.Range(0.0f, 99999.0f);
+        offsetY = Random.Range(0.0f, 99999.0f);
+    }
+
+    private void Update()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = GenerateTexture();
+    }
+    Texture2D GenerateTexture()
+    {
+        Texture2D texture = new Texture2D(width, height);
+        
+        for(int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Color colour = CalculateColour(x, y);
+                texture.SetPixel(x, y, colour);
+            }
+        }
+        texture.Apply();
+        return texture;
+    }
+    Color CalculateColour(int x, int y)
+    {
+        float xCoord = (float)x / width * scale + offsetX, yCoord = (float)y / height * scale + offsetY;
+        float sample = Mathf.PerlinNoise(xCoord, yCoord);
+        return new Color(sample, sample, sample);
+    }
+}
